84042704b8ba1dc34c708ea892c97d05bd5cd5a5
Emma <emmamartinezgarcia2806@gmail.com>
2024-09-02 17:05:43 +0200

    ejercicio terminado

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    shuffleChoices() {
+        let array = this.choices
+        for (let i=array.length-1; i>0;i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            [array[i], array [j]] = [array[j], array[i]];
+        }
+        return array
+    }
 
-    // 2. shuffleChoices()
 }
+
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining= timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion () {
+        return this.questions[this.currentQuestionIndex]
+    }
 
-    // 2. getQuestion()
+    moveToNextQuestion () {
+        this.currentQuestionIndex++
+    }
+
+    shuffleQuestions() {
+        let array = this.questions
+        for (let i=array.length-1; i>0;i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            [array[i], array [j]] = [array[j], array[i]];
+        }
+        return array;
+
+    }
+        
     
-    // 3. moveToNextQuestion()
+    checkAnswer(answer) {
+        if (answer) {
+            this.correctAnswers++
+        }
+    }
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
 }
+    
